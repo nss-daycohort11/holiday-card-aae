e399e84dcfdf9a3cd0aa0731f4b2213f91ecdfe6
asagraves <andrewsagraves@gmail.com>
2015-10-29 11:01:29 -0500

    inital commit

---
+++ vietnamese.js
@@ -0,0 +1,11 @@
+var Translate = (function(argument) {
+    var vietnamese = {"merry":"vui vẻ", "christmas":"Giáng Sinh", "and":"và", "happy":"hạnh phúc", "new":"mới", "year":"năm"};
+
+
+    argument.getVietnameseTranslation = function() {
+        var input = $("#message-input").val();
+        
+    };
+
+    return argument;
+})(Translate);
