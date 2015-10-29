9d9cb0dd3174ca58eb2f4f39326a1c3dfda1a04a
Emma Beaton <emma.beaton@gmail.com>
2015-10-29 10:57:22 -0500

    adding welsh.js and translateobject.js files

---
+++ welsh.js
@@ -0,0 +1,36 @@
+
+var Translation = (function(argument) {
+	var welsh = {"merry":"god", "christmas":"jul", "and":"och", "happy":"gott", "new":"nytt", "year":"år"};
+
+	return {
+		argument.getWelshTranslation = function() {
+			var input = $("#text-input").val();
+			
+		};
+
+	}
+})(Translate);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
